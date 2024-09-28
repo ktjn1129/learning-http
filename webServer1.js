fe774c318f081c9e92cbdd438a18c03008eae0fe
Jinichiro Koto <jinichirokoto@JinichironoMacBook-Pro.local>
2024-09-28 14:19:12 +0900

    update

--- webServer1.js
+++ webServer1.js
@@ -18,7 +18,18 @@ net
       const path = requestLine.split(" ")[1];
       console.log(path);
 
-      const fileContent = fs.readFileSync(`.${path}`);
+      const requestFile = path.endsWith("/") ? path + "index.html" : path;
+
+      if (!fs.existsSync(`.${requestFile}`)) {
+        const httpResponse = `HTTP/1.1 404 Not Found
+content-length: 0
+
+`;
+        socket.write(httpResponse);
+        return;
+      }
+
+      const fileContent = fs.readFileSync(`.${requestFile}`);
       const httpResponse = `HTTP/1.1 200 OK
 content-length: ${fileContent.length}
 
