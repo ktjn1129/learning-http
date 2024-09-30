44d81d3e3f071ccccb3c4ef8d5db4b990e546629
Jinichiro Koto <jinichirokoto@JinichironoMacBook-Pro.local>
2024-09-30 12:35:00 +0900

    update

---
+++ taskWebApp.js
@@ -0,0 +1,67 @@
+const http = require("http");
+
+const PORT = 8080;
+
+const tasks = [
+  {
+    title: "フロントエンドの実装",
+    createdAt: new Date(),
+  },
+  {
+    title: "サーバサイドの実装",
+    createdAt: new Date(),
+  },
+];
+
+function getTaskHTML() {
+  const tasksHTMLElement = tasks
+    .map((task) => {
+      return `<tr>
+	<td>${task.title}</td>
+	<td>${task.createdAt}</td>
+</tr>`;
+    })
+    .join("");
+
+  return `<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8" />
+    <title>tasks</title>
+  </head>
+  <body>
+    <h1>タスク一覧</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>タイトル</th>
+          <th>作成日時</th>
+        </tr>
+      </thead>
+      <tbody>
+				${tasksHTMLElement}
+      </tbody>
+    </table>
+  </body>
+</html>
+`;
+}
+
+http
+  .createServer((request, response) => {
+    const path = request.url;
+    console.log(`[request] ${path}`);
+
+    if (path === "/tasks") {
+      response.writeHead(200);
+      const responseBody = getTaskHTML();
+      response.write(responseBody);
+      response.end();
+      return;
+    }
+
+    response.writeHead(404);
+    response.end();
+    return;
+  })
+  .listen(PORT, "127.0.0.1");
